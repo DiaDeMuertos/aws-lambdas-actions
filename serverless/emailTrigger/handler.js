e5cdcfe3ae4b992335ee01bff737eda22ac370f9
jayceesnow <jcrodriguez@alioit.com>
2021-03-10 11:58:39 -0700

    added emailTrigger lambda

---
+++ serverless/emailTrigger/handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.emailTrigger = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'template message',
+    }),
+  };
+};
